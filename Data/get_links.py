1fd609e9a559ad12881acc32fd71d3a92aad24a9
Matthew Wong <mwon594@aucklanduni.ac.nz>
2026-09-24 12:12:18 +1200

    change data

--- Data/get_links.py
+++ Data/get_links.py
@@ -3,6 +3,11 @@
 2) pdf -> text file (key:value pair) - potentially as JSON.
 3) store each text file.
 '''
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-def get_pdf_links():
-    
+
+
+def get_pdf_links(base_url = ):
+    response = requests.get(base_url)
